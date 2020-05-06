2d2fa06555d187c0c0738b6a411d7cbec75a84f2
James Swank <jmswank7@gmail.com>
2020-05-06 13:34:31 -0600

    2nd program

---
+++ code_samples/008-hello-world/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello everyone, this is the most ")
+
+	foo()
+
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
+}
