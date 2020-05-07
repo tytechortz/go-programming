f17fad01ae3d6b34b1afc1c700757a16dab04b32
James Swank <jmswank7@gmail.com>
2020-05-07 13:06:50 -0600

    exercise 5

--- code_samples/exercises/level_1/main.go
+++ code_samples/exercises/level_1/main.go
@@ -6,10 +6,17 @@ type burrito int
 
 var x burrito
 
+var y int
+
 func main() {
+
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 
 }
