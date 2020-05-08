b551d4b3143b21fe642ce8795c8c7feeadc06d5a
James Swank <jmswank7@gmail.com>
2020-05-08 14:06:55 -0600

    correction

--- code_samples/control-flow/046-switch-statement/default-case/main.go
+++ code_samples/control-flow/046-switch-statement/default-case/main.go
@@ -8,21 +8,6 @@ func main()  {
 		fmt.Println("this should not print")
 	case (2 == 4):
 		fmt.Println("this should not print2")
-	case (3 == 3):
-		fmt.Println("prints")
-		fallthrough
-	case (4 == 4):
-		fmt.Println("also true, does it print?")
-		fallthrough
-	case (7 == 9):
-		fmt.Println("not true 1")
-		fallthrough
-	case (11 == 9):
-		fmt.Println("not true 2")
-		fallthrough
-	case (15 == 15):
-		fmt.Println("true 15")
-		fallthrough
 	default:
 		fmt.Println("this is default")
 	}
