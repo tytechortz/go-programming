773ff9c8ec3753e9089cd7b7284cedeeaf270f7b
James Swank <jmswank7@gmail.com>
2020-05-12 13:15:50 -0600

    more

--- code_samples/functions/variadic-parameters/main.go
+++ code_samples/functions/variadic-parameters/main.go
@@ -6,7 +6,15 @@ func main () {
 	foo(2,3,4,5,6,7,8,9)
 }
 
-func foo(x ...int) {
+func foo(x ...int) int {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
+
+	sum := 0
+	for i, v := range x {
+		sum += v
+		fmt.Println("For item in index position", i, "we are now adding", v, "to the total which is now", sum)
+	}
+	fmt.Println("The total is", sum)
+	return sum
 }
